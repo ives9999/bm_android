cba09ba6d10059faad2ac15ff3a39400820b7dce
ives <ives@housetube.tw>
2018-02-02 23:34:37 +0800

    no message

--- app/src/main/java/com/sportpassword/bm/Controllers/LoginActivity.kt
+++ app/src/main/java/com/sportpassword/bm/Controllers/LoginActivity.kt
@@ -1,13 +1,32 @@
 package com.sportpassword.bm.Controllers
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.Toolbar
 import com.sportpassword.bm.R
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        //setSupportActionBar(toolbar)
+        //supportActionBar!!.setTitle(resources.getText(R.string.login))
+
+        //supportActionBar!!.setDisplayShowTitleEnabled(false)
+        //setTitle(resources.getText(R.string.login))
+
+        //val t: Toolbar = find<Toolbar>(R.id.toolbar)
+        //val v: TextView = findViewById(R.id.toolbar_title) as TextView
+        val l = LayoutInflater.from(this).inflate(R.layout.activity_main, null, false)
+        //val toolbar: Toolbar = l.findViewById<Toolbar>(R.id.toolbar)
+        //println(toolbar)
+        val toolbar_title: TextView = l.findViewById<TextView>(R.id.toolbar_title)
+        toolbar_title.text = "登入"
+        supportActionBar!!.setCustomView(toolbar_title)
     }
 }
