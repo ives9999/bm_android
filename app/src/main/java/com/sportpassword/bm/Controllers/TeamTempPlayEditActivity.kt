64dbc0ce02c0bc76efd4e237c480dd0b4f940eab
ives sun <ives@housetube.tw>
2018-04-16 15:56:51 +0800

    set temp play edit status switch style complete.

--- app/src/main/java/com/sportpassword/bm/Controllers/TeamTempPlayEditActivity.kt
+++ app/src/main/java/com/sportpassword/bm/Controllers/TeamTempPlayEditActivity.kt
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.sportpassword.bm.R
+import kotlinx.android.synthetic.main.activity_team_temp_play_edit.*
 
 class TeamTempPlayEditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team_temp_play_edit)
+
+        onoff.isChecked = true
     }
 
     fun submit(view: View) {
