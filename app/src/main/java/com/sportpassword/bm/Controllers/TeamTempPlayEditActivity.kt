d2bff5b3e950025f59dbdaac0eb397ca5c67e9f3
ives <ives@housetube.tw>
2018-04-17 19:55:42 +0800

    set temp play status and quantity complete.

--- app/src/main/java/com/sportpassword/bm/Controllers/TeamTempPlayEditActivity.kt
+++ app/src/main/java/com/sportpassword/bm/Controllers/TeamTempPlayEditActivity.kt
@@ -19,38 +19,13 @@ class TeamTempPlayEditActivity : BaseActivity() {
     val model: Team = Team(0, "", "", "")
     var oldQuantity: Int = 0
     var oldStatus: Boolean = false
+    var isKeyboardShow: Boolean = false
+    var keyboardView: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team_temp_play_edit)
 
-        onoff.onCheckedChange { buttonView, isChecked ->
-            //println(isChecked)
-            if (isChecked) {
-                quantity_container.visibility = View.VISIBLE
-                model.temp_play_data[TEAM_TEMP_STATUS_KEY]!!["value"] = "on"
-            } else {
-                quantity_container.visibility = View.INVISIBLE
-                model.temp_play_data[TEAM_TEMP_STATUS_KEY]!!["value"] = "off"
-            }
-            if (isChecked != oldStatus) {
-                model.temp_play_data[TEAM_TEMP_STATUS_KEY]!!["change"] = true
-            }
-        }
-        temp_play_quantity.setOnFocusChangeListener { _v, hasFocus ->
-            val v = _v as EditText
-            if (!hasFocus) {
-                _hideKeyboard(v)
-                val quantity: Int = v.text.toString().toInt()
-                if (quantity != this.oldQuantity) {
-                    model.temp_play_data[TEAM_TEMP_QUANTITY_KEY]!!["value"] = quantity
-                    model.temp_play_data[TEAM_TEMP_QUANTITY_KEY]!!["change"] = true
-                }
-            } else {
-                v.setSelection(v.length())
-                _showKeyboard(v)
-            }
-        }
         teamToken = intent.getStringExtra("token")
         refresh()
     }
@@ -63,13 +38,36 @@ class TeamTempPlayEditActivity : BaseActivity() {
                     model.temp_play_data = TeamService.temp_play_data
                     //println(model.temp_play_data)
                     dataToField()
+                    temp_play_quantity.setSelection(temp_play_quantity.length())
                     l.dismiss()
+                    setEventListener()
                 }
             }
         }
     }
 
-    fun dataToField() {
+    private fun setEventListener() {
+        onoff.onCheckedChange { buttonView, isChecked ->
+            setStatus()
+        }
+        temp_play_quantity.setOnFocusChangeListener { _v, hasFocus ->
+            val v = _v as EditText
+            if (hasFocus && onoff.isChecked) {
+                v.setSelection(v.length())
+                _showKeyboard(v)
+                isKeyboardShow = true
+                keyboardView = v
+            } else {
+                _hideKeyboard(v)
+                setQuantity()
+            }
+        }
+        clearbutton_temp_play_quantity.setOnClickListener {
+            temp_play_quantity.setText("")
+        }
+    }
+
+    private fun dataToField() {
         val status = model.temp_play_data[TEAM_TEMP_STATUS_KEY]!!["value"] as String
         if (status == "on") {
             onoff.isChecked = true
@@ -86,7 +84,17 @@ class TeamTempPlayEditActivity : BaseActivity() {
         }
     }
 
+    private fun fieldToData() {
+        setStatus()
+        setQuantity()
+    }
+
     fun submit(view: View) {
+        if (isKeyboardShow && keyboardView != null) {
+            _hideKeyboard(keyboardView!!)
+        }
+        fieldToData()
+        //println(model.temp_play_data)
         val params: MutableMap<String, Any> = model.makeTempPlaySubmitArr()
         if (params.size == 0) {
             Alert.show(this, "提示", "沒有修改任何資料")
@@ -110,6 +118,29 @@ class TeamTempPlayEditActivity : BaseActivity() {
                     Alert.show(this, "錯誤", TeamService.msg)
                 }
 
+            }
+        }
+    }
+
+    private fun setStatus() {
+        val isChecked = onoff.isChecked
+        if (isChecked) {
+            quantity_container.visibility = View.VISIBLE
+            model.temp_play_data[TEAM_TEMP_STATUS_KEY]!!["value"] = "on"
+        } else {
+            quantity_container.visibility = View.INVISIBLE
+            model.temp_play_data[TEAM_TEMP_STATUS_KEY]!!["value"] = "off"
+        }
+        if (isChecked != oldStatus) {
+            model.temp_play_data[TEAM_TEMP_STATUS_KEY]!!["change"] = true
+        }
+    }
+    private fun setQuantity() {
+        if (temp_play_quantity.visibility == View.VISIBLE) {
+            val quantity: Int = temp_play_quantity.text.toString().toInt()
+            if (quantity != this.oldQuantity) {
+                model.temp_play_data[TEAM_TEMP_QUANTITY_KEY]!!["value"] = quantity
+                model.temp_play_data[TEAM_TEMP_QUANTITY_KEY]!!["change"] = true
             }
         }
     }
