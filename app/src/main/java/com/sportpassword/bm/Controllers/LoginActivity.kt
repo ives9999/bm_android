8c5c3ea6349ff898215036aad96aacf7d67dcce2
ives <ives@housetube.tw>
2018-04-21 01:38:43 +0800

    no message

--- app/src/main/java/com/sportpassword/bm/Controllers/LoginActivity.kt
+++ app/src/main/java/com/sportpassword/bm/Controllers/LoginActivity.kt
@@ -29,20 +29,6 @@ class LoginActivity : BaseActivity() {
 
         //loginEmailTxt.setText("ives@housetube.tw")
         //loginPasswordTxt.setText("K5SD23r6")
-        //setSupportActionBar(toolbar)
-        //supportActionBar!!.setTitle(resources.getText(R.string.login))
-
-        //supportActionBar!!.setDisplayShowTitleEnabled(false)
-        //setTitle(resources.getText(R.string.login))
-
-        //val t: Toolbar = find<Toolbar>(R.id.toolbar)
-        //val v: TextView = findViewById(R.id.toolbar_title) as TextView
-        val l = LayoutInflater.from(this).inflate(R.layout.activity_main, null, false)
-        //val toolbar: Toolbar = l.findViewById<Toolbar>(R.id.toolbar)
-        //println(toolbar)
-        val toolbar_title: TextView = l.findViewById<TextView>(R.id.toolbar_title)
-        toolbar_title.text = "登入"
-        supportActionBar!!.setCustomView(toolbar_title)
     }
 
     fun loginSubmit(view: View) {
