47ad41136bd67d9fa8df4651ff6eadf7c6a06acd
ives <ives@housetube.tw>
2018-05-29 12:46:21 +0800

    1.0.5 start.

--- app/src/main/java/com/sportpassword/bm/Controllers/ValidateActivity.kt
+++ app/src/main/java/com/sportpassword/bm/Controllers/ValidateActivity.kt
@@ -2,8 +2,11 @@ package com.sportpassword.bm.Controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
 import com.sportpassword.bm.R
+import com.sportpassword.bm.member
+import kotlinx.android.synthetic.main.activity_validate.*
 
 class ValidateActivity : BaseActivity() {
 
@@ -16,9 +19,16 @@ class ValidateActivity : BaseActivity() {
         type = intent.getStringExtra("type")
         if (type == "email") {
             setMyTitle("email認證")
+            typeLbl.text = "email"
+            typeTxt.setMyText(member.email, "")
+            typeTxt.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
         } else if (type == "mobile") {
             setMyTitle("手機認證")
+            typeLbl.text = "手機"
+            typeTxt.setMyText(member.mobile, "")
+            typeTxt.inputType = InputType.TYPE_CLASS_PHONE
         }
+        hidekeyboard(validate_layout)
     }
 
     fun submit(view: View) {
