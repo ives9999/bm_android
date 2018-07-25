aac141c271dcf37f093edd1eb05a887b00abe6b0
ives <ives@housetube.tw>
2018-07-26 00:16:41 +0800

    no message

--- app/src/main/java/com/sportpassword/bm/Models/SuperData.kt
+++ app/src/main/java/com/sportpassword/bm/Models/SuperData.kt
@@ -8,7 +8,7 @@ import kotlin.reflect.full.memberProperties
 /**
  * Created by ives on 2018/2/17.
  */
-open class Data(val id: Int, val title: String, val token: String, val featured_path: String="", val vimeo: String="", val youtube: String="") {
+open class SuperData(val id: Int, val title: String, val token: String, val featured_path: String="", val vimeo: String="", val youtube: String="") {
     open var data: MutableMap<String, MutableMap<String, Any>> = mutableMapOf()
 
     open fun dataReset(){}
