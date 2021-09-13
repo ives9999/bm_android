61e1a674c37b3d8099be4bc308beeaf2035b0ba3
ives <ives@housetube.tw>
2021-09-13 08:50:00 +0800

    1.7.1 start. update youtube player.

--- app/src/main/java/com/sportpassword/bm/Controllers/ShowTeachVC.kt
+++ app/src/main/java/com/sportpassword/bm/Controllers/ShowTeachVC.kt
@@ -1,22 +1,28 @@
 package com.sportpassword.bm.Controllers
 
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 import com.google.gson.JsonParseException
 import com.sportpassword.bm.Models.Table
 import com.sportpassword.bm.Models.TeachTable
 import com.sportpassword.bm.R
 import com.sportpassword.bm.Services.TeachService
 import com.sportpassword.bm.Utilities.jsonToModel
-import com.xwray.groupie.GroupAdapter
 import kotlinx.android.synthetic.main.activity_show_course_vc.*
 import kotlin.reflect.full.memberProperties
 import kotlinx.android.synthetic.main.activity_show_teach_vc.*
 import kotlinx.android.synthetic.main.activity_show_teach_vc.refresh
-import kotlinx.android.synthetic.main.activity_show_teach_vc.tableView
+import com.google.android.youtube.player.YouTubePlayerView
 
-class ShowTeachVC : ShowVC() {
+class ShowTeachVC : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
     var myTable: TeachTable? = null
+    val api_key = "AIzaSyCR_6_3cnKPtamzWcXtwqjedFCcKYD3zPI"
+
+    var youTubePlayer: YouTubePlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -35,8 +41,86 @@ class ShowTeachVC : ShowVC() {
             "pv" to hashMapOf( "icon" to "pv","title" to "瀏覽數","content" to ""),
             "created_at_show" to hashMapOf( "icon" to "calendar","title" to "建立日期","content" to "")
         )
+        youtube.initialize(api_key, this)
 
         refresh()
+    }
+
+    override fun onInitializationSuccess(
+        provider: YouTubePlayer.Provider?,
+        youTubePlayer: YouTubePlayer?,
+        wasRestored: Boolean
+    ) {
+        youTubePlayer?.setPlayerStateChangeListener(playerStateChangeListener)
+        youTubePlayer?.setPlaybackEventListener(playbackEventListener)
+
+        this.youTubePlayer = youTubePlayer
+
+//        if (!wasRestored) {
+//            youTubePlayer?.cuePlaylist("")
+//        }
+    }
+
+    override fun onInitializationFailure(
+        provider: YouTubePlayer.Provider?,
+        youTubeInitializationResult: YouTubeInitializationResult?
+    ) {
+        val REQUEST_CODE = 0
+
+        if (youTubeInitializationResult?.isUserRecoverableError == true) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show()
+        } else {
+            val errorMessage = "youtube player 初始化發生錯誤 ($youTubeInitializationResult)"
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private val playbackEventListener = object: YouTubePlayer.PlaybackEventListener {
+        override fun onSeekTo(p0: Int) {
+
+        }
+
+        override fun onBuffering(p0: Boolean) {
+
+        }
+
+        override fun onPlaying() {
+            Toast.makeText(this@ShowTeachVC, "影片播放正常", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onStopped() {
+            Toast.makeText(this@ShowTeachVC, "影片停止", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onPaused() {
+            Toast.makeText(this@ShowTeachVC, "影片暫停", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val playerStateChangeListener = object: YouTubePlayer.PlayerStateChangeListener {
+        override fun onAdStarted() {
+            Toast.makeText(this@ShowTeachVC, "按下廣告", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onLoading() {
+
+        }
+
+        override fun onVideoStarted() {
+            Toast.makeText(this@ShowTeachVC, "影片開始播放", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onLoaded(p0: String?) {
+
+        }
+
+        override fun onVideoEnded() {
+            Toast.makeText(this@ShowTeachVC, "影片結束播放", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onError(p0: YouTubePlayer.ErrorReason?) {
+
+        }
     }
 
     override fun genericTable() {
@@ -73,8 +157,13 @@ class ShowTeachVC : ShowVC() {
             }
         }
 
-        youtube.settings.javaScriptEnabled = true
-        youtube.loadUrl("https://www.youtube.com/embed/" + myTable!!.youtube)
+        //youtube.settings.javaScriptEnabled = true
+        //youtube.loadUrl("https://www.youtube.com/embed/" + myTable!!.youtube)
+
+        if (youTubePlayer != null) {
+            youTubePlayer!!.loadVideo(myTable!!.youtube)
+            youTubePlayer!!.play()
+        }
 
         val items = generateMainItem()
         adapter.update(items)
