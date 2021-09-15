3394efbc5e22248721040a7b303fc3e2c2ebe0a4
ives <ives@housetube.tw>
2021-09-15 08:58:15 +0800

    1.7.2 start, update member structure.

---
+++ app/src/main/java/com/sportpassword/bm/Models/MemberTable.kt
@@ -0,0 +1,14 @@
+package com.sportpassword.bm.Models
+
+class MemberTable: Table() {
+
+    var nickname: String = ""
+    var dob: String = ""
+    var sex: String = ""
+    var email: String = ""
+    var city: Int = -1
+    var area: Int = -1
+    var road: String = ""
+    var zip: Int = 0
+    var a: String = ""
+}
