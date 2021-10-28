9777ee3e87126ca3983771dea9e0629b4fd9e269
ives <ives@housetube.tw>
2021-10-28 14:30:50 +0800

    no message

--- app/src/main/java/com/sportpassword/bm/Controllers/ArenaVC.kt
+++ app/src/main/java/com/sportpassword/bm/Controllers/ArenaVC.kt
@@ -8,81 +8,117 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sportpassword.bm.Fragments.MyViewHolder
 import com.sportpassword.bm.Models.*
 import com.sportpassword.bm.R
 import com.sportpassword.bm.Services.ArenaService
+import com.sportpassword.bm.Services.CourseService
 import com.sportpassword.bm.Utilities.*
 import kotlinx.android.synthetic.main.activity_arena_vc.*
+import kotlinx.android.synthetic.main.activity_arena_vc.page_refresh
+import kotlinx.android.synthetic.main.activity_course_vc.*
 import kotlinx.android.synthetic.main.arena_list_cell.view.*
 import kotlinx.android.synthetic.main.bottom_view.*
+import kotlinx.android.synthetic.main.olcell.view.*
 import kotlinx.android.synthetic.main.top_view.*
 import org.jetbrains.anko.backgroundColor
 
-class ArenaVC : AppCompatActivity() {
+class ArenaVC : MyTableVC() {
 
     var mysTable: ArenasTable? = null
     lateinit var tableAdapter: ArenaAdapter
+
+    lateinit var youAdapter: YouAdapter
+    val youItems: ArrayList<YouItem> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_arena_vc)
-        val myColorGreen = ContextCompat.getColor(this, R.color.MY_GREEN)
-        arenaTabLine.backgroundColor = myColorGreen
-        topTitleLbl.setText("課程")
 
-        tableAdapter = ArenaAdapter()
-        val a1: ArenaTable = ArenaTable()
-        a1.title = "aaa"
-        val a2: ArenaTable = ArenaTable()
-        a2.title = "bbb"
-        val a: ArrayList<ArenaTable> = arrayListOf(
-            a1, a2
-        )
-        tableAdapter.tableList = a
+        this.dataService = ArenaService
+        able_type = "arena"
 
-        //refresh()
+        refreshLayout = page_refresh
+
+
+        youAdapter = YouAdapter()
+        youAdapter.items = youItems
+//        val l = LinearLayoutManager(this)
+//        l.orientation = LinearLayoutManager.VERTICAL
+//        list.layoutManager = l
+        list.adapter = youAdapter
+//        val myColorGreen = ContextCompat.getColor(this, R.color.MY_GREEN)
+//        arenaTabLine.backgroundColor = myColorGreen
+//        topTitleLbl.setText("課程")
+//
+//        tableAdapter = ArenaAdapter()
+//        val a1: ArenaTable = ArenaTable()
+//        a1.title = "aaa"
+//        val a2: ArenaTable = ArenaTable()
+//        a2.title = "bbb"
+//        val a: ArrayList<ArenaTable> = arrayListOf(
+//            a1, a2
+//        )
+//        tableAdapter.tableList = a
+
+        refresh()
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        isSearchIconShow = true
-//        super.onCreateOptionsMenu(menu)
-//
-//        return true
-//    }
-
-
-//    override fun generateItems(): ArrayList<Item> {
-//        if (mysTable != null) {
-//            for (row in mysTable!!.rows) {
-//                //row.print()
-//                row.filterRow()
-//                val myItem = ArenaItem(this, row)
-//                //val coachItem = CoachItem(this, row)
-//                myItem.list1CellDelegate = this
-//                items.add(myItem)
-//            }
-//        }
-//
-//        return items
-//    }
-
-
-
-//    override fun rowClick(item: com.xwray.groupie.Item<com.xwray.groupie.GroupieViewHolder>, view: View) {
-//
-//        val arenaItem = item as ArenaItem
-//        val table = arenaItem.row
-//        //superCourse.print()
-//        toShowArena(table.token)
-//    }
-
+    override fun genericTable() {
+        mysTable = jsonToModels<ArenasTable>(jsonString!!)
+        if (mysTable != null) {
+            tables = mysTable
+            getPage()
+//            tableLists += generateItems1(CourseTable::class, mysTable!!.rows)
+//            tableAdapter.setMyTableList(tableLists)
+            for (i in 0..5) {
+                youItems.add(YouItem(i.toString()))
+            }
+            youAdapter.items = youItems
+            runOnUiThread {
+                youAdapter.notifyDataSetChanged()
+//                tableAdapter.notifyDataSetChanged()
+            }
+        }
+    }
 
 }
 
-class ArenaAdapter(): RecyclerView.Adapter<ArenaViewHolder>() {
+data class YouItem(val name: String)
+
+class YouAdapter: RecyclerView.Adapter<YouAdapter.ViewHolder>() {
+
+    var items: ArrayList<YouItem> = arrayListOf()
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val name: TextView = itemView.name
+
+        fun bind(item: YouItem) {
+            name.text = item.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val example = inflater.inflate(R.layout.you, parent, false)
+        return ViewHolder(example)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
+
+
+
+class ArenaAdapter: RecyclerView.Adapter<ArenaViewHolder>() {
 
     var tableList: ArrayList<ArenaTable> = arrayListOf()
 //class ArenaAdapter(resource: Int, list1CellDelegate: List1CellDelegate?): MyAdapter<ArenaViewHolder>(resource, ::ArenaViewHolder, list1CellDelegate) {
